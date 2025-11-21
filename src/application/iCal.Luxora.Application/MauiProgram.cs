9ea90fce1e342f566a4094925fbeb49c647de1be
AAcherdantsev <75489700+AAcherdantsev@users.noreply.github.com>
2025-11-21 20:09:44 +0100

    Add new dependencies and enhance theming;

--- src/application/iCal.Luxora.Application/MauiProgram.cs
+++ src/application/iCal.Luxora.Application/MauiProgram.cs
@@ -3,17 +3,23 @@ using UraniumUI;
 using CommunityToolkit.Maui;
 
 namespace iCal.Luxora.Application;
+
 public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
-        {
-            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            fonts.AddFontAwesomeIconFonts();
-        }).UseUraniumUI().UseUraniumUIMaterial().UseMauiCommunityToolkit();
+        builder.UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFontAwesomeIconFonts();
+            })
+            .UseUraniumUI()
+            .UseUraniumUIBlurs()
+            .UseUraniumUIMaterial()
+            .UseMauiCommunityToolkit();
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
