3ce8c0f84da93c703c706d3ac9960dcf68dfc393
AAcherdantsev <75489700+AAcherdantsev@users.noreply.github.com>
2025-11-24 02:04:19 +0100

    Update EffectParameterView with IntSliderBehavior, add new icons, and modify FoundDevice layout;

---
+++ src/application/iCal.Luxora.Application/UI/Common/Behaviors/IntSliderBehavior.cs
@@ -0,0 +1,25 @@
+namespace iCal.Luxora.Application.UI.Common.Behaviors;
+
+public class IntSliderBehavior : Behavior<Slider>
+{
+    protected override void OnAttachedTo(Slider bindable)
+    {
+        base.OnAttachedTo(bindable);
+        bindable.ValueChanged += OnSliderValueChanged;
+    }
+
+    protected override void OnDetachingFrom(Slider bindable)
+    {
+        base.OnDetachingFrom(bindable);
+        bindable.ValueChanged -= OnSliderValueChanged;
+    }
+
+    private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        var slider = (Slider)sender;
+        int rounded = (int)Math.Round(e.NewValue);
+
+        if (slider.Value != rounded)
+            slider.Value = rounded;
+    }
+}
