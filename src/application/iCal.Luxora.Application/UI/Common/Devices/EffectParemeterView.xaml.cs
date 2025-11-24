4d8a471106fed9e91d66090e447662b096afa779
AAcherdantsev <75489700+AAcherdantsev@users.noreply.github.com>
2025-11-24 01:40:07 +0100

    Add EffectParemeterView;

---
+++ src/application/iCal.Luxora.Application/UI/Common/Devices/EffectParemeterView.xaml.cs
@@ -0,0 +1,9 @@
+namespace iCal.Luxora.Application.UI.Common.Devices;
+
+public partial class EffectParemeterView : ContentView
+{
+    public EffectParemeterView()
+    {
+        InitializeComponent();
+    }
+}
